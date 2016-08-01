0b726353d91b9103b89eeede4cb4eef16c33d3c2
danksalot <danksalot@users.noreply.github.com>
2016-08-01 13:30:48 -0500

    Create Puzzle1.js

---
+++ Puzzle1.js
@@ -0,0 +1,18 @@
+function doubleInteger(i) {
+    
+    // i will be an integer. Double it and return it.
+    
+    return 2 * i;
+    
+}
+
+/*
+
+Unit Tests:
+doubleInteger(2);   // 4
+doubleInteger(4);   // 8
+doubleInteger(-10); // -20
+doubleInteger(0);   // 0
+doubleInteger(100); // 200
+
+*/
