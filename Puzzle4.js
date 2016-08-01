fca4c0635aba0aad4a983e0844cf734a703b6bd6
danksalot <danksalot@users.noreply.github.com>
2016-08-01 13:39:06 -0500

    Create Puzzle4.js

---
+++ Puzzle4.js
@@ -0,0 +1,19 @@
+function longestString(i) {
+    
+    // i will be an array.
+    // return the longest string in the array
+    
+    return i.reduce(function(longest, current) { return typeof current == 'string' && current.length > longest.length ? current : longest }, "")
+    
+}
+
+/*
+
+Unit Tests:
+longestString(['a','ab','abc']);                              // abc
+longestString(['big',[0,1,2,3,4],'tiny']);                    // tiny
+longestString(['Hi','World','你好']);                         // world
+longestString([true,false,'lol']);                            // lol
+longestString([{object: true,mainly: 'to confuse you'},'x']); // x
+
+*/
