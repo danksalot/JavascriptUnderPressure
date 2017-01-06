04dd4fe4c8abbb31bdfb928043b3297b67b83f34
danksalot <danksalot@users.noreply.github.com>
2017-01-06 11:38:35 -0600

    Update minimized.js

--- minimized.js
+++ minimized.js
@@ -9,6 +9,8 @@ return i.split('.')[1] || false
 
 // Problem 4
 return i.reduce(function (l,x) { return typeof x == 'string' && x.length > l.length? x : l }, "")
+// Shorter:
+return i.reduce((l,c)=>typeof c=='string'&&c.length>l.length?c:l,"")
 
 // Problem 5
 return i.map(function (x) { return x instanceof Array ? arraySum(x) : typeof x == 'number'? x : 0 }).reduce(function (x,y) {return x+y})
